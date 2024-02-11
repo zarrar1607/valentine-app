2509f53fe0a3b26b99b4c5d340c78443e4434b6f
zarrar1607 <zarrar1607@gmail.com>
2024-02-11 01:40:49 -0600

    cheanged the phrases array

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ import { useState } from "react";
 const phrases = [
   "No",
   "Are you sure?",
+  "Please reconsider!",
   "Really sure?",
   "Pookie Please",
+  "I'll make you my princess",
   "Don't do this to me",
   "I'm gonna cry...",
   "You're breaking my heart ;(",
-  "Think of the love!",
-  "I'll make you my princess",
-  "Let's write our love story together!",
-  "Please reconsider!",
 ];
 
 function App() {
   const [no_cnt, setNoCount] = useState(0);
   const [yes_pressed, setYesPressed] = useState(0);
-  const yes_btn_size = no_cnt * 20 + 16;
+  const yes_btn_size = no_cnt * 20 + 40;
 
   function handleNoClick() {
     setNoCount(no_cnt + 1);
@@ -40,7 +38,7 @@ function App() {
             </img>
 
             <div className='text'>
-              Yay!!!
+              <h1>Yay!!!💖</h1>
             </div>
           </>
 
@@ -52,20 +50,36 @@ function App() {
             >
             </img>
             <div>
-              Will you be my Valentine💘?
+              <h1>Will you be my Valentine💘?</h1>
             </div>
             <div>
               <button
                 className='yes-btn'
-                style={{ fontSize: yes_btn_size }}
+                style={{ 
+                  backgroundColor: 'green',
+                  color: 'white',
+                  fontSize: yes_btn_size ,
+                  borderRadius: '2rem',
+                  border: 'none',
+                  paddingLeft:'20px',
+                  paddingRight:'20px'
+                }}
                 onClick={() => setYesPressed(true)}
               >
-                yes
+                Yes
               </button>
 
               <button
                 onClick={handleNoClick}
                 className='no-btn'
+                style={{ 
+                  backgroundColor: 'red',
+                  color: 'white',
+                  borderRadius: '2rem',
+                  border: 'none',
+                  paddingLeft:'20px',
+                  paddingRight:'20px'
+                }}
               >
                 {getNoBtnTxt()}
               </button>
